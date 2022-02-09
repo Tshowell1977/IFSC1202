1f9753e10fe586cc4c41d8ade5b0ef14f571b758
Tshowell1977 <tshowell@ualr.edu>
2022-02-09 03:06:42 +0000

    commit

---
+++ 04.03 Product of N Numbers.py
@@ -0,0 +1,6 @@
+N=input("Enter Number: ")
+prod=1
+for x in range(1,int(N)+1):
+    prod *=x
+    
+print(prod)
---
+++ 04.04 Number of Zeros.py
@@ -0,0 +1,9 @@
+N=input("Enter N: ")
+count=0
+for x in range(1,int(N)+1):
+    x=int(input("Enter Number: "))
+    if x == 0:
+        xx=1
+        count +=xx
+    
+print(count)
