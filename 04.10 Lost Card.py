519201405efa6399cc628e18e7b0db3eabeb9106
Tshowell1977 <tshowell@ualr.edu>
2022-02-15 01:30:36 +0000

    commit

--- 04.10 Lost Card.py
+++ 04.10 Lost Card.py
@@ -1,15 +1,22 @@
-# Read an integer:
-# a = int(input())
-# Print a value:
-# print(a)
-answer=0
-N=int(input("Enter Number of Cards :"))
-N_orginal=N 
-for i in range(N-1):
-   
-  a=int(input("Enter Card: "))
-for i in range(N_orginal):
-  N_orginal+=i
-  i+=N
-  N_org-=answer
-print(N_orginal)
+N=int(input('Enter Number of Cards:'))
+
+#reading each card and creating an array containing them
+
+x=0
+for i in range(1,N):
+  x=int(input('Enter Card:'))
+
+
+#sum of the values from 1 to N (Total Value of Cards)
+s1=0
+for i in range(1,N+1):
+  s1=s1+i
+
+#sum of the values on remaining cards (sum of elements of array A)
+s2=0
+for i in range(len(A)):
+  s2=s2+A[i]
+
+#value on the lost card
+s=s1-s2
+print('Missing Card:', s)
---
+++ 05.01 Sum of Sequence.py
@@ -0,0 +1,6 @@
+N=input("Enter a Number ")
+prod=1(Zero to Quit)
+for x in range(1,int(N)+1):
+    prod *=x
+    
+print(prod)
