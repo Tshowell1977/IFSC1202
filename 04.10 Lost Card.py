7b625e76c7e8b0b7fa3a0ec37ea96a9a5e8c6520
Tshowell1977 <tshowell@ualr.edu>
2022-02-15 03:15:06 +0000

    commit

--- 04.10 Lost Card.py
+++ 04.10 Lost Card.py
@@ -6,11 +6,14 @@ for i in range(1,N+1):
   #print(ct)
 #counting the total of entered cards  
   ct1=0
-for j in range(1,N):
-  ct1=int(input("Enter Number :"))
-  ct1=ct1+j
-  print(ct1)
+for j in range(N-1):
+  count=int(input("Enter Number : "))
+  ct1+= count
+  #print(ct1)
 
+
+d=ct-ct1
+print("Missing Card: " + str(d))
   
 
 
