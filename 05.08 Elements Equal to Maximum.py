644f0d61dda6a343726e66d1d81882efe0252d66
Tshowell1977 <tshowell@ualr.edu>
2022-02-16 03:39:39 +0000

    commit

--- 05.08 Elements Equal to Maximum.py
+++ 05.08 Elements Equal to Maximum.py
@@ -1,16 +1,14 @@
-ct=0
+ct=1
 max=0
-eqmax=0
 while 1:
     n = float(input("Enter a Value (zero to quit):"))  
     if n ==0:
         print("Maximum: ",max)
-        print("number of occurences: ",eqmax)
+        print("Number of occurences: ",ct)
         break
     else:
-        ct +=1
-        
+        if n==max:         
+            ct +=1
         if n > max:
-            
-                max=n
-        
+            max=n
+       
