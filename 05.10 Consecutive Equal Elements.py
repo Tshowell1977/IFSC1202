df00a9b25e036912f3884b8ad4656e7e18d04686
Tshowell1977 <tshowell@ualr.edu>
2022-02-16 15:08:42 +0000

    commit

--- 05.10 Consecutive Equal Elements.py
+++ 05.10 Consecutive Equal Elements.py
@@ -1,14 +1,23 @@
-n1=0
-n2=0
-ct=1
-while 1:
-    n = int(input('Enter a Number (zero to quit): '))
-    if n == 0: 
-        break
-    else:
-        if n1!=n:
-            n1=n
+n = 1
+previous = 0
+i = 0
+ct = 0
+x = None
+maxct = 0
+while n != 0:
+    n = int(input("Enter a Number (zero to quit): "))
+    if n != 0:
+        if i == 0:
+            previous = n
+            ct = 1
         else:
-            ct+=ct
-    n2=ct
-print(n1,'repeated', n2,'times')
+            if n == previous:
+                ct += 1
+            else:
+                if ct > maxct:
+                    maxct = ct
+                    x = previous
+        i += 1
+        previous = n
+
+print(x, "repeated", maxct, "times")
