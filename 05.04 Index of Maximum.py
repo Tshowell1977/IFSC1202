3f146860fcfb537345be39191a4f39c608f015ac
Tshowell1977 <tshowell@ualr.edu>
2022-02-16 02:42:35 +0000

    commit

--- 05.04 Index of Maximum.py
+++ 05.04 Index of Maximum.py
@@ -4,9 +4,11 @@ while 1:
     n = float(input("Enter a Value (zero to quit):"))  
     if n ==0:
         print("Maximum: ",max)
+        print("Index of Maximum: ",index)
         break
     else:
         ct +=1
         
         if n > max:
-            max=n
+            max=n 
+            index=ct
---
+++ 05.05 Number of Even Values.py
@@ -0,0 +1,12 @@
+ct=0
+max=0
+while 1:
+    n = float(input("Enter a Value (zero to quit):"))  
+    if n == 0:
+        print("Number of Even Values: ",ct)
+        break
+    else:
+                
+        if n/2 == 0:
+            ct=ct+1
+            
