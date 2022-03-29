54b7e37332788ddca8e558baf80067df80c93de8
Tshowell1977 <tshowell@ualr.edu>
2022-03-29 23:45:45 +0000

    commit

--- 9.0/09.01 Maximim Of A Two Dimensional List.py
+++ 9.0/09.01 Maximim Of A Two Dimensional List.py
@@ -1,6 +1,36 @@
 
-# input for rows and columns
-print("Enter the number of rows and columns:")
-r = int(input())
-c = int(input())
 
+
+print("Enter the number of rows and columns: ")
+m = int(input())
+n = int(input())
+
+matrix = []
+
+for i in range(m):          
+    l = []
+    print("Enter a line of data:")
+    for j in range(n):      
+        l.append(int(input()))
+    matrix.append(l)
+   
+for i in range(m):          
+    for j in range(n):      
+        print(matrix[i][j],end = " ")
+    print()
+
+max = 0
+for i in range(m):          
+    for j in range(n):  
+        if max < matrix[i][j]:
+            max = matrix[i][j]
+
+a = 0
+b = 0
+for i in range(m):          
+    for j in range(n):  
+        if max == matrix[i][j]:
+            a = i
+            b = j
+            print("The maximum value",max,"occurred in row",a,"column",b)
+            break
--- 9.0/09.06 City Temperatures Project.py
+++ 9.0/09.06 City Temperatures Project.py
@@ -1,34 +1,28 @@
-# opening 09.06 CityTemps.txt file in reading mode
-with open("/workspace/IFSC1202/9.0/09.06 CityTemps.txt", 'r') as file:
-    temps = []  # initializing temps list to store data
 
-    # reading first line of input file
+with open("/workspace/IFSC1202/9.0/09.06 CityTemps.txt", 'r') as file:
+    temps = []
+
     line = file.readline()
 
-    # using while loop to read through each line
     while line:
-
-        # spitting line into list by taking space as delimiter
+     
         row = line.split()
-        temps.append(row)  # appending list to temps list
-        line = file.readline()   # read next line
+        temps.append(row)  
+        line = file.readline()   
 
-# using for loop to go through each city temperatures
 for i in range(len(temps)):
     total = 0    
 
     for temp in temps[i][1:]:
-        total = total + float(temp)   # adding temperature to total
-
-    # calculating average
+        total = total + float(temp)  
+   
     average = total / (len(temps[i]) - 1)
-    temps[i].append(int(average))  # appending average to temps list
+    temps[i].append(int(average))  
 
-# printing  heading of table
-print(f"{'City' : <10}{'Mo' : <10}{'Tu' : <10}{'We' : <10}{'Th' : <10}{'Fr' : <10}{'Sa' : <10}{'Su' : <10}{'Avg' : <10}")
-  
-# printing data in table form using f-string 
+print(f"{'City' : <12}{'Mo' : <12}{'Tu' : <12}{'We' : <12}{'Th' : <12}{'Fr' : <12}{'Sa' : <12}{'Su' : <12}{'Avg' : <12}")
+ 
+ 
 for i in range(len(temps)):
     for j in range(len(temps[0])):
-        print(f"{temps[i][j] : <10}", end='')
+        print(f"{temps[i][j] : <12}", end='')
     print("")
